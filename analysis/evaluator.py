15ba743c194ff85f8e3b61488d2745d5e539b0e1
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2021-07-01 22:39:46 -0300

    Refatorando o codigo: Agora é feito um strip em todas as palavras antes de tokenizar

--- analysis/evaluator.py
+++ analysis/evaluator.py
@@ -1,7 +1,9 @@
+import pandas as pd
+
 class Evaluator:
     def __init__(self, dataset=None):
         if dataset is not None:
             self.df = pd.read_csv(dataset, usecols=["skill", "backward_text"])
 
-    def load_dataset(dataset):
+    def load_dataset(self, dataset):
         self.df = pd.read_csv(dataset, usecols=["skill", "backward_text"])
